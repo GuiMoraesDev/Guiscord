9225697336cbaaa0a65d267a88fcdac8003e48f4
Guilherme Moraes <guimoraes.dev@gmail.com>
2022-01-29 21:19:34 -0300

    fix: fix github api url

--- src/services/github/api.tsx
+++ src/services/github/api.tsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://github.com/',
+  baseURL: 'https://api.github.com',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
 });
 
 export default api;
